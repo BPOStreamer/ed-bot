3fd7f722cf043faedac3b79a791ab86a77151062
Anh <demonxranger8@gmail.com>
2021-01-17 13:35:43 +0700

    yeyeyeyey

---
+++ main.js
@@ -0,0 +1,13 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://ed.engdis.com/iighighschools#/home');
+    await driver.findElement(By.name('password')).sendKeys('12345', Key.RETURN);
+    await driver.findElement(By.name('userName')).sendKeys('n05Anh10N1', Key.RETURN);
+    await driver.sleep(5000);
+  } finally {
+    await driver.quit();
+  }
+})();
