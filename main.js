503f587eeafbe41e108ce5eb1edcbb839627e541
Anh <demonxranger8@gmail.com>
2021-01-19 22:06:36 +0700

    main

--- main.js
+++ main.js
@@ -1,13 +1,61 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver'),
+      chrome = require('selenium-webdriver/chrome'),
+      screen = {
+        width: 1600,
+        height: 900
+      }
 
-async function init() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  try {
-    await driver.get('https://ed.engdis.com/iighighschools#/home');
-    await driver.findElement(By.name('password')).sendKeys('12345', Key.RETURN);
-    await driver.findElement(By.name('userName')).sendKeys('n05Anh10N1', Key.RETURN);
-    await driver.sleep(5000);
-  } finally {
-    await driver.quit();
+module.exports = {
+
+init: async function(username, password) {
+  let driver = await new Builder()
+                            .forBrowser('chrome')
+                            .setChromeOptions(new chrome.Options().headless().windowSize(screen).addArguments('--remote-debugging-port=9222'))
+                            .build()
+  const notifCloseButton = driver.findElement(By.className('notificationsCenter_hideSlide')),
+        browserTitle = driver.getTitle();
+
+  let isNotifClosed = false;
+   
+  await driver.get('https://ed.engdis.com/iighighschools#/home');
+  await driver.findElement(By.name('password')).sendKeys(password, Key.RETURN);
+  await driver.findElement(By.name('userName')).sendKeys(username, Key.RETURN);
+  //await driver.sleep(5000);
+  //await driver.quit();
+  while (isNotifClosed == false) {
+    setInterval(2000, async function() {
+        if ((await notifCloseButton).isDisplayed) {
+          (await notifCloseButton).click();
+          scrapeUserData();
+          isNotifClosed = true;
+        } else {
+          scrapeUserData();
+          isNotifClosed = true;
+        }
+    })
   }
+
+  async function scrapeUserData() {
+     let xmlReq = new XMLHttpRequest(), userDataJSON = 0;
+
+     xmlReq.open('GET', "https://ed.engdis.com/WebApi/Progress/GetDefaultCourseProgress", false);
+
+     xmlReq.onreadystatechange = function() {
+      if(xmlReq.readyState == 4 && xmlReq.status == 200) {
+        return xmlReq.responseText;
+      }
+     }
+
+     xmlReq.send();
+  }
+
+},
+
+completeUnit: async function() {
+  console.log('tba');
+}
+
 };
+
+//init()
+require('make-runnable');
