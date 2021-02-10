de3b19fe9d8f395e1ca6313554cc01903ea987cc
Anh <demonxranger8@gmail.com>
2021-02-10 23:02:22 +0700

    update

--- main.js
+++ main.js
@@ -1,61 +1,68 @@
-const {Builder, By, Key, until} = require('selenium-webdriver'),
+const {Builder, By, Key, until, Actions, Browser} = require('selenium-webdriver'),
       chrome = require('selenium-webdriver/chrome'),
       screen = {
         width: 1600,
         height: 900
       }
 
+let driver = new Builder()
+                    .forBrowser('chrome')
+                    .setChromeOptions(new chrome.Options().headless().windowSize(screen).addArguments('--remote-debugging-port=9222'))//.addArguments('--disable-web-security'))
+                    .build(),
+    notifCloseButton = 0,
+    loginButton = 0,
+    progressTextBox = 0,
+    data = 0,
+    homeOkBtn = 0,
+    actions = driver.actions({async: false});
+
 module.exports = {
 
-init: async function(username, password) {
-  let driver = await new Builder()
-                            .forBrowser('chrome')
-                            .setChromeOptions(new chrome.Options().headless().windowSize(screen).addArguments('--remote-debugging-port=9222'))
-                            .build()
-  const notifCloseButton = driver.findElement(By.className('notificationsCenter_hideSlide')),
-        browserTitle = driver.getTitle();
-
-  let isNotifClosed = false;
-   
+init: async function(username, password, unitNum) {
   await driver.get('https://ed.engdis.com/iighighschools#/home');
+  loginButton = await driver.findElement(By.id('submit1'))
+  await driver.wait(until.elementIsVisible(loginButton));
   await driver.findElement(By.name('password')).sendKeys(password, Key.RETURN);
   await driver.findElement(By.name('userName')).sendKeys(username, Key.RETURN);
-  //await driver.sleep(5000);
-  //await driver.quit();
-  while (isNotifClosed == false) {
-    setInterval(2000, async function() {
-        if ((await notifCloseButton).isDisplayed) {
-          (await notifCloseButton).click();
-          scrapeUserData();
-          isNotifClosed = true;
-        } else {
-          scrapeUserData();
-          isNotifClosed = true;
-        }
-    })
+
+  await driver.wait(until.elementLocated(By.xpath('/html/body/div/app-root/notifications-center-carousel/div/div/div[1]')));
+  notifCloseButton = await driver.findElement(By.xpath("/html/body/div/app-root/notifications-center-carousel/div/div/div[1]"));
+  await driver.wait(until.elementIsVisible(notifCloseButton));
+
+  //selenium will do the scraping because anyorigin cant be used for dynamic content, bummer.
+  await driver.get('https://ed.engdis.com/WebApi/Progress/GetDefaultCourseProgress');
+
+  progressTextBox = await driver.findElement(By.xpath('/html/body/pre'));
+  await progressTextBox.getText().then (function(text) {
+    data = JSON.parse(text)
+    console.log('\x1b[33m%s\x1b[0m', `${data.CourseProgressTree.Name} \n-----------------`)
+
+    for (var i = 0; i < data.CourseProgressTree.Children.length; i++) {
+      console.log('\x1b[33m%s\x1b[0m', `${data.CourseProgressTree.Children[i].Name} (progress:  ${data.CourseProgressTree.Children[i].Progress}%, lessons in unit: ${data.CourseProgressTree.Children[i].Children.length})`)
+    }   
+  })
+
+  await driver.get('https://ed.engdis.com/iighighschools#/home');
+
+  homeOkBtn = await driver.findElements(By.name('btnOk'));
+  if (homeOkBtn.length == 1) {
+    await actions.click(homeOkBtn[0]).perform();
   }
 
-  async function scrapeUserData() {
-     let xmlReq = new XMLHttpRequest(), userDataJSON = 0;
+  completeUnit(unitNum)
 
-     xmlReq.open('GET', "https://ed.engdis.com/WebApi/Progress/GetDefaultCourseProgress", false);
-
-     xmlReq.onreadystatechange = function() {
-      if(xmlReq.readyState == 4 && xmlReq.status == 200) {
-        return xmlReq.responseText;
-      }
-     }
-
-     xmlReq.send();
-  }
-
-},
-
-completeUnit: async function() {
-  console.log('tba');
 }
-
 };
 
-//init()
+async function completeUnit(unitNum) {
+  let unitName = data.CourseProgressTree.Children[unitNum - 1].Name;
+  
+  await driver.findElement
+
+  for (var i = 0; i < data.CourseProgressTree.Children[unitNum - 1].Children.length; i++) {
+      console.log(data.CourseProgressTree.Children[unitNum - 1].Children[i].Metadata.Code)
+  }
+}
+
+console.log('running')
 require('make-runnable');
